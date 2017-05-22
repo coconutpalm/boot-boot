f8ed71335c6947dcfe370c961525d88e5cd06656
David Orme <djo@coconut-palm-software.com>
2017-05-22 12:59:33 -0500

    Start string tests

---
+++ test/clj_boot/string_test.clj
@@ -0,0 +1,11 @@
+(ns clj-boot.string-test
+  (:require [clojure.test :refer :all]
+            [clj-boot.string :refer :all]))
+
+
+;; Waiting for => from clj-foundation to finish
+
+(testing "merge-strings"
+  (testing ""))
+
+(testing "delimited-words")
