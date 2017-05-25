4cfc1af3cccab500d5e0ad8188cc954bcd454a61
David Orme <djo@coconut-palm-software.com>
2017-05-24 19:22:03 -0500

    Add specs

--- src/clj_boot/string.clj
+++ src/clj_boot/string.clj
@@ -1,33 +1,23 @@
 (ns clj-boot.string
   (:require [clojure.string :as str]
-            [clojure.spec.alpha :as s]))
-
+            [clojure.spec.alpha :as s]
+            [clj-foundation.fn-spec :refer [=>]]))
 
 
 (^:private def delimeters [\' \"])
 (^:private def delimeter-set (set delimeters))
 
-
 (s/def ::word-vector     (s/coll-of string?))
-(s/def ::string-or-nil   #(or (string? %)
-                              (nil? %)))
 (s/def ::maybe-delimeter #(or (delimeter-set %)
                               (nil? %)))
 (s/def ::merge-result    (s/tuple ::word-vector ::maybe-delimeter string?))
 
 
-(defn merge-strings
+(=> merge-strings [::word-vector ::maybe-delimeter string? string?] ::merge-result
   "Given a vector of strings, merge strings beginning/ending with quotes into
   a single string and return a vector standalone words and quoted strings.
   Nested / unbalanced quotes will return undefined results."
   [[result delimeter merging] next]
-
-  {:pre [(s/valid? ::word-vector     result)
-         (s/valid? ::maybe-delimeter delimeter)
-         (s/valid? string?           merging)
-         (s/valid? string?           next)]}
-
-  {:post [#(s/valid? ::merge-result %)]}
 
   (let [start (first (seq next))
         end   (last (seq next))]
@@ -39,10 +29,9 @@
       (nil? delimeter)               [(conj result next) nil ""]
       :else                          [result delimeter (str merging " " next)])))
 
-#_(=> merge-strings [::word-vector ::maybe-delimeter string? string?] ::merge-result)
 
 
-(defn delimited-words
+(=> delimited-words [string?] ::word-vector
   "Split a string into words, respecting single or double quoted substrings.
   Nested quotes are not supported.  Unbalenced quotes will return undefined
   results."
